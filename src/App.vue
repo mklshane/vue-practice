590e700f54d836aff19c977f0669d45bdc1e958f
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-10-26 16:58:31 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Test Title" subtitle="Test Subtitle"/>
+  <Hero/>
+  <HomeCards />
+  <JobListings />
 </template>
